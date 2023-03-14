548fc7dd7357f6beb961b9a9b59b8a43355fb570
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-03-14 17:41:26 +0100

    ADD RELATIONS TABLE TO DATABASE SCHEMA

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_14_144934) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_14_163902) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_14_144934) do
   create_table "groups", force: :cascade do |t|
     t.string "name"
     t.string "icon"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "relations", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
