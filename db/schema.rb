602cc72f1c0b3287e5ac200a9912a9085e3d9071
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-03-14 15:51:19 +0100

    ADD ENTITIES TABLE TO THE DATABASE SCHEMA

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_14_143427) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_14_144934) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "entities", force: :cascade do |t|
+    t.string "name"
+    t.integer "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "groups", force: :cascade do |t|
     t.string "name"
