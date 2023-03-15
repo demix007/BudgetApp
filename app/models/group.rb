51a6bf82d4276df9ed8ae732c1ed51ae791318db
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-03-15 16:43:34 +0100

    ADD self_total METHOD INTO THE GROUP MODEL

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,6 +5,17 @@ class Group < ApplicationRecord
   validates :icon, presence: true
   validates :name, presence: true, length: { in: 3..300 }
 
+  def self_total(id)
+    group = Group.find(id)
+    relations = group.relations
+    @total = 0
+    relations.each do |relation|
+      entity = Entity.find(relation.entity_id)
+      @total += entity.amount
+    end
+    @total
+  end
+  
   def created_at
     attributes['created_at'].strftime('%d %b %Y')
   end
