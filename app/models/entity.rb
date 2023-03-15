6fcf5344f49abd26c5444d65d4c2ca0e22621bbd
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-03-15 16:42:31 +0100

    ADD self_total METHOD INTO THE ENTITY MODEL

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -4,6 +4,17 @@ class Entity < ApplicationRecord
   validates :name, presence: true, length: { in: 3..300 }
   validates :amount, numericality: { greater_than: 0 }
 
+  def self_total(id)
+    group = Group.find(id)
+    relations = group.relations
+    @total = 0
+    relations.each do |relation|
+      entity = Entity.find(relation.entity_id)
+      @total += entity.amount
+    end
+    @total
+  end
+  
   def created_at
     attributes['created_at'].strftime('%d %b %Y')
   end
