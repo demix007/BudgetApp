0c4812bc65c2a0c58596e4c08f444bc96a6c923b
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-03-15 07:48:17 +0100

    ADD created_at METHOD TO THE ENTITY MODEL

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,4 +1,8 @@
 class Entity < ApplicationRecord
     belongs_to :user
     has_many :relations, dependent: :destroy
+
+    def created_at
+      attributes['created_at'].strftime('%d %b %Y')
+    end
 end
