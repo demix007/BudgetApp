8d98e8e36d95db26baade0f0ad2c6c311dec1429
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-03-15 13:56:57 +0100

    ADD AMOUNT VALIDATION TO THE ENTITY MODEL

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -2,6 +2,7 @@ class Entity < ApplicationRecord
   belongs_to :user
   has_many :relations, dependent: :destroy
   validates :name, presence: true, length: { in: 3..300 }
+  validates :amount, numericality: { greater_than: 0 }
 
   def created_at
     attributes['created_at'].strftime('%d %b %Y')
