b6fbbf4ea1f0bc2b2e01d7927c135465a3016fe5
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-03-15 16:20:14 +0100

    ADD LOGIC INTO THE create METHOD OF THE ENTITIES CONTROLLER

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -9,5 +9,16 @@ class EntitiesController < ApplicationController
   end
 
   def create
+    entity = current_user.entities.new(entity_params)
+    respond_to do |format|
+      format.html do
+        if entity.save
+          relation = Relation.create(group_id: group_params, entity_id: entity.id)
+          redirect_to group_path(relation.group_id)
+        else
+          render :new
+        end
+      end
+    end
   end
 end
