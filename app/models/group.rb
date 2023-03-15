62f3dfed0473e227be17a211401f183c80f8c7f9
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-03-15 12:59:44 +0100

    ADD NAME VALIDATION TO GROUP MODEL

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,7 @@ class Group < ApplicationRecord
   has_many :relations, dependent: :destroy
   belongs_to :user
   validates :icon, presence: true
+  validates :name, presence: true
 
   def created_at
     attributes['created_at'].strftime('%d %b %Y')
