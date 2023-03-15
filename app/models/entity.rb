48bab4afb5f589d7e388cfb8fd25f105eded013b
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-03-15 13:56:41 +0100

    ADD NAME VALIDATION TO THE ENTITY MODEL

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,6 +1,7 @@
 class Entity < ApplicationRecord
   belongs_to :user
   has_many :relations, dependent: :destroy
+  validates :name, presence: true, length: { in: 3..300 }
 
   def created_at
     attributes['created_at'].strftime('%d %b %Y')
