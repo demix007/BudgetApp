a3df80e06df028fcf8ffca06280f2068082ca674
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-03-15 12:58:03 +0100

    ADD TESTS TO user_spec.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   has_many :entities, dependent: :destroy
   has_many :groups, dependent: :destroy
   validates :name, presence: true
+  validates :name, presence: true, length: { in: 2..300 }
 end
