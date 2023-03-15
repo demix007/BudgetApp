aaaacac340dc2b841a0bc68a6618849cf6d619e3
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-03-15 16:26:57 +0100

    ADD LOGIC INTO THE SHOW ACTION OF THE GROUPS CONTROLLER

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,7 +3,10 @@ class GroupsController < ApplicationController
     @groups = current_user.groups.all
   end
 
-  def show; end
+  def show
+    @group = Group.find(params[:id])
+    @relations = @group.relations.order('created_at DESC')
+  end
 
   def new; end
 end
