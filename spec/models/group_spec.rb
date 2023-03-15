c9cde2344e9945aa00d20c811bbec33522d53a80
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-03-15 13:50:46 +0100

    ADD TEST TO VALIDATE NAME PRESENCE IN THE GROUP MODEL

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe Group, type: :model do
     @user1 = User.create! name: 'Ayodeji Abidemi'
     @group = @user1.groups.create! name: 'Fastfood', icon: 'icon'
   end
+
+  it 'name should be present' do
+    @group.name = nil
+    expect(@group).to_not be_valid
+  end
+
 end
