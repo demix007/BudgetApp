5e00b2a92832977c3cde7509159980cdc7b4fc43
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-03-15 07:50:22 +0100

    ADD created_at METHOD TO THE GROUP MODEL

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,8 @@
 class Group < ApplicationRecord
     has_many :relations, dependent: :destroy
     belongs_to :user
+
+    def created_at
+      attributes['created_at'].strftime('%d %b %Y')
+    end
 end
