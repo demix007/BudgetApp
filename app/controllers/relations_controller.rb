c383a877cab1db14321f5ff080b6c7dae1377960
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-03-15 16:39:14 +0100

    ADD LOGIC INTO THE new METHOD OF RELATIONS CONTROLLER

--- app/controllers/relations_controller.rb
+++ app/controllers/relations_controller.rb
@@ -1,3 +1,7 @@
 class RelationsController < ApplicationController
-  def new; end
+  def new
+    @group = Group.all
+    @entity = current_user.entities.new
+    @relation = Relation.new
+  end
 end
