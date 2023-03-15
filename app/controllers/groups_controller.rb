f7023ec4ea97eb38d7639e339c9cbe9ddecb550a
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-03-15 16:27:37 +0100

    ADD LOGIC INTO THE new ACTION OF THE GROUPS CONTROLLER

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,10 +3,12 @@ class GroupsController < ApplicationController
     @groups = current_user.groups.all
   end
 
+  def new
+    @group = current_user.groups.new
+  end
+  
   def show
     @group = Group.find(params[:id])
     @relations = @group.relations.order('created_at DESC')
   end
-
-  def new; end
 end
