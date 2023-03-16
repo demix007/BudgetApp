31025aef3c69ecab31f3e21a8e8e6eda51b8150b
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-03-16 18:44:50 +0100

    ADD CONFIRMABLE TO DEVISE

--- app/model/user.rb
+++ app/model/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :groups, dependent: :destroy
   has_many :entities, dependent: :destroy
   validates :name, presence: true
