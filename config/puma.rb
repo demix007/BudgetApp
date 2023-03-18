446459958c0b85d172332c9c8e14fe56517c33d7
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-03-18 14:59:08 +0100

    MODIFY puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -29,7 +29,6 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # the concurrency of the application would be max `threads` * `workers`.
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
-#
 # workers ENV.fetch("WEB_CONCURRENCY") { 4 }
 
 # Use the `preload_app!` method when specifying a `workers` number.
